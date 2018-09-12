470d0bf448e0870d5991b8f6d0118fbda2540b7b
Tim Williams <tmw@autotrain.org>
2018-09-12 21:17:27 +0100

    Force 25fps in recording

--- record.sh
+++ record.sh
@@ -13,15 +13,16 @@ rm -f recordings/current.txt
 
 echo $filename > recordings/current.txt
 
-ffmpeg -f v4l2 \
+ffmpeg -f v4l2 -framerate 25 \
  -i /dev/video1 \
  -f pulse -i default \
- -vcodec libx264 -vb 4000k -acodec aac \
- -x264-params keyint=50:scenecut=0 \
- -preset superfast \
+ -vcodec libx264 -vb 5000k -acodec aac \
+ -x264-params keyint=25:scenecut=0 \
+ -preset ultrafast \
  -vf scale=1280x720 \
  -ab 256k \
+ -bsf:v h264_mp4toannexb \
  -movflags empty_moov+omit_tfhd_offset+frag_keyframe+default_base_moof \
  -async 1 -vsync 1 \
- -segment_list_flags +live -hls_allow_cache 0  -hls_time 2  -hls_wrap 50 \
+ -segment_list_flags +live -hls_allow_cache 0  -hls_time 1  -hls_wrap 50 \
  -f tee  -map 0:0 "live/video.m3u8|recordings/$filename"
