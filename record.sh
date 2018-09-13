648488f2a79e0c88d1fdc791e82358438cc32ce5
Tim Williams <tmw@autotrain.org>
2018-09-13 21:40:53 +0100

    Add thread queue size and video size

--- record.sh
+++ record.sh
@@ -13,8 +13,11 @@ rm -f recordings/current.txt
 
 echo $filename > recordings/current.txt
 
-ffmpeg -f v4l2 -framerate 25 \
+
+ffmpeg -f v4l2 -framerate 25 -video_size 720x576 \
+ -thread_queue_size 512  \
  -i /dev/video1 \
+ -thread_queue_size 512  \
  -f pulse -i default \
  -vcodec libx264 -vb 5000k -acodec aac \
  -x264-params keyint=25:scenecut=0 \
@@ -23,7 +26,6 @@ ffmpeg -f v4l2 -framerate 25 \
  -ab 256k \
  -async 1 -vsync 1 \
  -bsf:v h264_mp4toannexb \
- -async 1 -vsync 1 \
  -movflags empty_moov+omit_tfhd_offset+frag_keyframe+default_base_moof \
  -segment_list_flags +live -hls_allow_cache 0  -hls_time 1  -hls_wrap 50 \
  -f tee  -map 0:0 -map 1:0 "live/video.m3u8|recordings/$filename"
