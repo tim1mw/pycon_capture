71519914148788de0b819c8cc3053fd57a940968
Mark Einon <einonm@cf.ac.uk>
2018-09-22 21:47:57 +0100

    Make the refresh_schedule.sh script a little more robust
    Signed-off-by: Mark Einon <einonm@cf.ac.uk>

--- refresh_schedule.sh
+++ refresh_schedule.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+mkdir -p recordings
 rm -f recordings/schedule.json
-wget -O recordings/schedule.json https://2018.hq.pyconuk.org/schedule/json/ 
+wget -O recordings/schedule.json https://2018.hq.pyconuk.org/schedule/json/
+if [ $? -ne 0 ]; then
+    echo "FAILED TO UPDATE SCHEDULE"
+fi
