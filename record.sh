cceab8301d8bb41ee86cf769b0543aa95391b6d0
Tim Williams <tmw@autotrain.org>
2019-09-09 16:48:55 +0100

    The image overlay broke the audio, fixing.

--- record.sh
+++ record.sh
@@ -14,17 +14,18 @@ rm -f recordings/current.txt
 echo $filename > recordings/current.txt
 
 
-ffmpeg -f v4l2 -framerate 25 -video_size 720x576 \
+ffmpeg -f pulse -i default \
+  -f v4l2 -framerate 25 -video_size 720x576 \
  -thread_queue_size 512  \
  -i /dev/video1 \
  -itsoffset 0.35 \
  -thread_queue_size 512  \
  -i images/logo.png \
- -f pulse -i default \
  -vcodec libx264 -vb 5000k -acodec aac \
  -x264-params keyint=25:scenecut=0 \
  -preset ultrafast \
  -filter_complex "scale=1280:720, overlay=0:0" \
+ -map 0:a \
  -ab 256k \
  -async 1 -vsync 1 \
  -bsf:v h264_mp4toannexb \
