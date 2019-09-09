2e479c1c98738b547696e94c259c8b0f9829a2a8
Tim Williams <tmw@autotrain.org>
2019-09-09 16:29:20 +0100

    Add an image overlay with the pycon logo

--- record.sh
+++ record.sh
@@ -19,11 +19,12 @@ ffmpeg -f v4l2 -framerate 25 -video_size 720x576 \
  -i /dev/video1 \
  -itsoffset 0.35 \
  -thread_queue_size 512  \
+ -i images/logo.png \
  -f pulse -i default \
  -vcodec libx264 -vb 5000k -acodec aac \
  -x264-params keyint=25:scenecut=0 \
  -preset ultrafast \
- -vf scale=1280x720 \
+ -filter_complex "scale=1280:720, overlay=0:0" \
  -ab 256k \
  -async 1 -vsync 1 \
  -bsf:v h264_mp4toannexb \
